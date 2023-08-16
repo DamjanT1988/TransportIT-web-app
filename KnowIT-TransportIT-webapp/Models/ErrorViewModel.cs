dffc246115ef80402ed230a4799c790ad60ef7c3
DamjanT1988 <Damjan_t@hotmail.com>
2023-08-16 16:59:21 +0200

    Add project files.

---
+++ KnowIT-TransportIT-webapp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KnowIT_TransportIT_webapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
